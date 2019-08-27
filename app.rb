83bfe3eec0e74b772a66d0836fe1afac8f5e14a9
Nicole Dow <nikki.dow71@gmail.com>
2019-08-27 17:44:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,8 +7,8 @@ class App < Sinatra::Base
   end
   
   post '/piglatinize' do 
-    @text = params[:user_phrase]
-    erb :piglatinize
+    # @text = params[:user_phrase]
+    # erb :piglatinize
   end
   
 end
